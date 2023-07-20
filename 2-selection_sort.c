55700ab8104cf2e5f486e827aeb404d8a56b3b08
Kholoudxs55kh <kfattem@gmail.com>
2023-07-20 16:47:41 -0700

    3rdTask

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,24 +9,24 @@
 
 void selection_sort(int *array, size_t size)
 {
-    int temp;
-    size_t min, i = 0, j;
+	int temp;
+	size_t min, i = 0, j;
 
-    for (; i < size; i++)
-    {
-        min = i;
+	for (; i < size; i++)
+	{
+		min = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
-        if (min != i)
-        {
-            temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            print_array(array, size);
-        }
-    }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 }
