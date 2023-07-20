1b8bc7ac8f4fd8f7319475c76b552b3a0a601985
Abodawoud <yousefabodawoud@gmail.com>
2023-07-21 00:18:09 +0300

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: The list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *temp, *sorted;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	curr = (*list)->next;
+	while (curr)
+	{
+		sorted = curr;
+		curr = curr->next;
+
+		while (sorted && sorted->prev && sorted->prev->n > sorted->n)
+		{
+			temp = sorted->prev;
+			sorted->prev = temp->prev;
+			temp->next = sorted->next;
+
+			if (temp->prev)
+				temp->prev->next = sorted;
+
+			if (sorted->next)
+				sorted->next->prev = temp;
+
+			temp->prev = sorted;
+			sorted->next = temp;
+
+			if (!sorted->prev)
+				*list = sorted;
+
+			print_list(*list);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -21,4 +21,8 @@ void swap(int *array, size_t curr, size_t next);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_first_nodes(listint_t **list, listint_t *curr, listint_t *temp);
+void swap_middle_nodes(listint_t **list, listint_t *curr, listint_t *temp);
+void swap_nodes(listint_t **list, listint_t *curr, listint_t *temp);
 #endif
