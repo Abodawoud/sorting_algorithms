4f4089d7aa06c5576728a317beced18a30e2f2e9
Kholoudxs55kh <kfattem@gmail.com>
2023-07-21 19:04:28 -0700

    DoneMandatory

---
+++ 3-quick_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+
+void swapp(int *array, size_t size, int i, int j);
+/**
+ * find_pivot - a function that is used to find the pivot
+ * @array: arr of ints
+ * @size: Size of array
+ * @start: first index in the array or sub-array
+ * @last: last index in the array or sub-array
+ * Return: the pivot's index
+ */
+int find_pivot(int *array, size_t size, int start, int last)
+{
+	int i = start - 1, j = start;
+
+	for (; j < last; j++)
+	{
+		if (array[j] < array[last])
+		{
+			i++;
+			swapp(array, size, i, j);
+		}
+	}
+	swapp(array, size, i + 1, last);
+	return (i + 1);
+}
+
+/**
+ * make_partitions_with_pivot - a function that is
+ * used to make partitions with the pivot [low, piv, high]
+ * @array: arr of ints
+ * @size: Size of array
+ * @start: first index in the array or sub-array
+ * @last: last index in the array or sub-array
+ * Return: void
+ */
+void make_partitions_with_pivot(int *array, size_t size, int start, int last)
+{
+	int pivot;
+
+	if (start > last)
+		return;
+
+	pivot = find_pivot(array, size, start, last);
+	make_partitions_with_pivot(array, size, start, pivot - 1);
+	make_partitions_with_pivot(array, size, pivot + 1, last);
+}
+
+/**
+ * swapp - swaps two eles
+ *
+ * @array: array of ints
+ * @size: size of th arr
+ * @i: first idx
+ * @j: sec idx
+ * Return: void
+ */
+void swapp(int *array, size_t size, int i, int j)
+{
+	int temp;
+
+	temp = array[j];
+	if (array[i] != array[j])
+	{
+		array[j] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
+}
+/**
+ * quick_sort - function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ *
+ * @array: The Array
+ * @size: The size of array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array)
+		make_partitions_with_pivot(array, size, 0, size - 1);
+}
